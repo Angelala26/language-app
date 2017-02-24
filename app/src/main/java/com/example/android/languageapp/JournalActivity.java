f149e2d2fc061f3978e127176a1d633d8dd9f83c
Angela Karl <angela@allwedo.com>
2017-02-24 14:48:35 +0100

    Fix logic in cycling through questions (still has some problems)

--- app/src/main/java/com/example/android/languageapp/JournalActivity.java
+++ app/src/main/java/com/example/android/languageapp/JournalActivity.java
@@ -4,25 +4,36 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import static com.example.android.languageapp.R.id.journalQuestion;
 
 /**
  * Created by Angela on 2/24/17.
  */
 
-public class JournalActivity extends AppCompatActivity{
+public class JournalActivity extends AppCompatActivity {
 
-    public TextView journalQuestion;
-    public int counter;
+    //set label for journal questions
+    public TextView journalQuestionLabel;
+    public int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journal);
 
+        // Call logic for the back button
+        goBackButton();
 
+        // Call logic for next question button
+        nextQuestionButton();
+    }
+
+    private void goBackButton() {
         // Set button to go back to opening page
         Button goBackToJournalExercises = (Button) findViewById(R.id.backToJournalExercises);
         goBackToJournalExercises.setOnClickListener(new View.OnClickListener() {
@@ -32,11 +43,16 @@ public class JournalActivity extends AppCompatActivity{
             }
         });
 
+    }
+
+    //TODO: this logic is messed up - the question starts blank, not with the first question
+    // and the last question won't show up so I added a random one at the end
+    private void nextQuestionButton() {
         // Set button to go to the next question (an arrary)
         // Initialize the journal question label
-        journalQuestion = (TextView) findViewById(R.id.journalQuestion);
+        journalQuestionLabel = (TextView) findViewById(journalQuestion);
 
-        // Connect with the button
+        // Connect with the next question button
         Button nextQuestionButton = (Button) findViewById(R.id.nextButton);
 
         // Get the questions array from the resources
@@ -46,14 +62,14 @@ public class JournalActivity extends AppCompatActivity{
         // Set up the onClickListener with the questions and next button
         nextQuestionButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                journalQuestion.setText(presentQuestionArray[counter]);
-                if(counter < (presentQuestionArray.length)) {
+                journalQuestionLabel.setText(presentQuestionArray[counter]);
+                if (counter < (presentQuestionArray.length - 1)) {
                     counter++;
-                }
-                else {
-                    journalQuestion.setText("Sorry, no more questions.");
+                } else {
+                    journalQuestionLabel.setText("Sorry, no more questions.");
                 }
             }
         });
     }
+
 }
